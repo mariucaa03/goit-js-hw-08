ffc92c50071a530de8eea9101de8f57e5d4549c6
mariuca <zahariuc_mariuca@yahoo.com>
2024-05-12 12:49:50 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,38 +8,29 @@ console.log(galleryItems);
 const galleryStart = document.querySelector(".gallery");
 
 function createImage() {
-let galleryHTML = "";
+    let galleryHTML = "";
 
-for (let i = 0; i < galleryItems.length; i++){
-galleryHTML += `
-<li class="gallery__item">
-<a class="gallery__link" href="${galleryItems[i].original}">
-<img class="gallery__image"
-src="${galleryItems[i].preview}"
-alt="${galleryItems[i].description}"
-/>
-</a>
-</li>`;
-}
-galleryStart.innerHTML = galleryHTML;
+    for (let i = 0; i < galleryItems.length; i++){
+        galleryHTML += `
+         <li class = "gallery__item">
+            <a class = "gallery__link" href = "${galleryItems[i].original}">
+                <img class = "gallery__image"
+                src = "${galleryItems[i].preview}"
+                data-source = "${galleryItems[i].original}"
+                alt = "${galleryItems[i].description}"
+                />
+            </a>
+        </li>`
+    }
+    galleryStart.innerHTML = galleryHTML;
 }
 
 createImage();
 
-let lightbox = null;
 
-galleryStart.addEventListener("click", e => {
-e.preventDefault();
-
-const imageURL = e.target.dataset.source;
-
-lightbox = new SimpleLightbox(`<img src="${imageURL}" width="800" height="600"/>`);
-lightbox.show();
-}
-);
-
-document.addEventListener("keydown", e => {
-lightbox.close();
-lightbox = null;
-}
-);
+document.addEventListener('DOMContentLoaded', function () {
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250
+    });
+  });
