6b58262688fe1fa16317e548502438bb4a3830b2
mariucaa03 <zahariuc_mariuca@yahoo.com>
2024-05-15 11:00:02 +0300

    upgrade

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,16 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
 
-const galleryStart = document.querySelector(".gallery");
+const galleryStart = document.querySelector('.gallery');
 
 function createImage() {
-    let galleryHTML = "";
+  let galleryHTML = '';
 
-    for (let i = 0; i < galleryItems.length; i++){
-        galleryHTML += `
+  for (let i = 0; i < galleryItems.length; i++) {
+    galleryHTML += `
          <li class = "gallery__item">
             <a class = "gallery__link" href = "${galleryItems[i].original}">
                 <img class = "gallery__image"
@@ -20,17 +19,16 @@ function createImage() {
                 alt = "${galleryItems[i].description}"
                 />
             </a>
-        </li>`
-    }
-    galleryStart.innerHTML = galleryHTML;
+        </li>`;
+  }
+  galleryStart.innerHTML = galleryHTML;
 }
 
 createImage();
 
-
 document.addEventListener('DOMContentLoaded', function () {
-    const lightbox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250
-    });
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
   });
+});
